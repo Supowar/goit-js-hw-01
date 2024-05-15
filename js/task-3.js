be48332b73932fa66274ca30c150394dce7ab033
Supowar <152823865+Supowar@users.noreply.github.com>
2024-05-15 11:28:08 +0300

    add4343

--- js/task-3.js
+++ js/task-3.js
@@ -5,11 +5,12 @@ function getElementWidth(content, padding, border) {
   const borderWidth = parseFloat(border);
 
   // Розраховуємо загальну ширину елемента
-  const totalWidth = contentWidth + paddingWidth + borderWidth;
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
 
   return totalWidth;
 }
 
+// Перевірка роботи функції
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
