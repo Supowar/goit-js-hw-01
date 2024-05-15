e50509609b3a63ce5c6191079df3dde0d299929d
Supowar <152823865+Supowar@users.noreply.github.com>
2024-05-15 09:23:37 +0300

    addJSnew

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,8 @@
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
+}
+
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
-console.log(getShippingMessage("Ukraine", 1000, 500)); // "Shipping to Ukraine will cost 1500 credits"
-console.log(getShippingMessage("USA", 2000, 100)); // "Shipping to USA will cost 2100 dits"
-console.log(getShippingMessage("Germany", 1500, 200)); // "Shipping to Germacreny will cost 1700 credits"
--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,15 @@
+function getElementWidth(content, padding, border) {
+  // Видаляємо "px" і перетворюємо значення на числа
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+
+  // Розраховуємо загальну ширину елемента
+  const totalWidth = contentWidth + paddingWidth + borderWidth;
+
+  return totalWidth;
+}
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
-const contentWidth = parseFloat(content);
-const paddingWidth = parseFloat(padding);
-const borderWidth = parseFloat(border);
-console.log(getElementWidth("100px", "10px", "5px")); // 115
-console.log(getElementWidth("200px", "20px", "10px")); // 230
-console.log(getElementWidth("50px", "5px", "2px")); // 57
